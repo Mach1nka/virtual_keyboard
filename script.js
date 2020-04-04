228f77e1726f9981caa1e168d2faafc475b73f9e
Artem <uspenskiyart@gmail.com>
2020-04-04 19:09:11 +0300

    feat: add functional for  shift

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const KEY_CODE_REGULAR = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187,
 const ENGLISH_KEYS =["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/","▲","◄","▼","►"];
 const SHIFT_ENGLISH_KEYS =["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "{", "}", "|", "A", "S", "D", "F", "G", "H", "J", "K", "L", ":", '"', "Z", "X", "C", "V", "B", "N", "M", "<", ">", "?", "▲","◄","▼","►"];
 const RUSSIAN_KEYS =["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "\\", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".","▲","◄","▼","►"];
-const SHIFT_RUSSIAN_KEYS =["Shift", "Ё", '"', "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "/", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ",","▲","◄","▼","►"];
+const SHIFT_RUSSIAN_KEYS =["Ё", '"', "№", ";", "%", ":", "?", "*", "(", ")", "_", "+", "Й", "Ц", "У", "К", "Е", "Н", "Г", "Ш", "Щ", "З", "Х", "Ъ", "/", "Ф", "Ы", "В", "А", "П", "Р", "О", "Л", "Д", "Ж", "Э", "Я", "Ч", "С", "М", "И", "Т", "Ь", "Б", "Ю", ",","▲","◄","▼","►"];
 const IRREGULAR_KEYS = ["Backspace", "Tab", "Del", "CapsLock", "Enter", "Shift", "Shift", "Ctrl", "Win", "Alt", "", "Alt","Ctrl" ];
 const CODE =["Backquote", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9", "Digit0", "Minus", "Equal", "Backspace", "Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Backslash", "Delete", "CapsLock", "KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ", "KeyK", "KeyL", "Semicolon", "Quote", "Enter", "ShiftLeft", "KeyZ", "KeyX", "KeyC", "KeyV", "KeyB", "KeyN", "KeyM", "Comma", "Period", "Slash", "ArrowUp", "ShiftRight", "ControlLeft", "MetaLeft", "AltLeft", "Space", "AltRight", "ArrowLeft", "ArrowDown", "ArrowRight", "ControlRight"];
 
@@ -55,8 +55,9 @@ function changeRegister(state/*, array*/) {
     })
     KEY_REGULAR.forEach((item) => {
       item.insertAdjacentText('afterbegin', ENGLISH_KEYS[i++].toUpperCase())
-      stateOfCaps = true;
     });
+    stateOfCaps = true;
+    document.querySelector('.caps').style.color = 'blue';
   }else if (state) {
     KEY_REGULAR.forEach((item) =>{
       item.innerText = '';
@@ -64,6 +65,7 @@ function changeRegister(state/*, array*/) {
     KEY_REGULAR.forEach((item) => {
       item.insertAdjacentText('afterbegin', ENGLISH_KEYS[i++])
     });
+    document.querySelector('.caps').style.color = '';
     stateOfCaps = false;
   }
 }
@@ -75,53 +77,65 @@ document.addEventListener('keyup', function (event) {
   document.querySelector('[code="'+event.code+'"]').classList.remove('-focus');
 })
 
-
-
+document.addEventListener('keyup', function (event) {
+  if (event.code == 'ShiftLeft' || event.code == 'ShiftRight') {
+    changeSymbols(ENGLISH_KEYS);
+  }
+})
 
 
 
 document.onkeydown = function (event) {
    event.preventDefault();
-   switch (event.code) {
-     case 'ControlLeft':
-      return null;
-      break;
-     case 'Delete':
-      return null;
-      break;
-     case 'ControlRight':
-      return null;
-      break;
-     case 'AltRight':
-      return null;
-      break;
-     case 'ShiftRight':
-       return null;
-       break;
-     case 'MetaLeft':
-       return null;
-       break;
-     case 'AltLeft':
-      return null;
-      break;
-     case 'Enter':
-      TEXTAREA.value += '\n';
-      break;
-     case 'Tab':
-      TEXTAREA.value += '  '
-      break;
-     case 'Backspace':
-      let arrayOfSymbols= document.getElementById('textarea').value.split('');
-      arrayOfSymbols.pop();
-      TEXTAREA.value = arrayOfSymbols.join('');;
-      break;
-      case 'Space':
-       TEXTAREA.value += ' ';
-       break;
-      case 'CapsLock':
-      changeRegister(stateOfCaps);
-        break;
-     default:
-     TEXTAREA.value += document.querySelector('[data="'+event.keyCode+'"]').innerText;
+   if (event) {
+     CODE.forEach(item => {
+       if (item != event.code) {
+         return null;
+       }else{
+         switch (event.code) {
+           case 'ControlLeft':
+            return null;
+            break;
+           case 'Delete':
+            return null;
+            break;
+           case 'ControlRight':
+            return null;
+            break;
+           case 'AltRight':
+            return null;
+            break;
+           case 'MetaLeft':
+             return null;
+             break;
+           case 'AltLeft':
+            return null;
+            break;
+           case 'Enter':
+            TEXTAREA.value += '\n';
+            break;
+           case 'Tab':
+            TEXTAREA.value += '  '
+            break;
+           case 'Backspace':
+            let arrayOfSymbols= document.getElementById('textarea').value.split('');
+            arrayOfSymbols.pop();
+            TEXTAREA.value = arrayOfSymbols.join('');;
+            break;
+            case 'Space':
+             TEXTAREA.value += ' ';
+             break;
+            case 'CapsLock':
+            changeRegister(stateOfCaps);
+              break;
+            case 'ShiftLeft':
+            case 'ShiftRight':
+            changeSymbols(SHIFT_ENGLISH_KEYS);
+              break;
+           default:
+           TEXTAREA.value += document.querySelector('[data="'+event.keyCode+'"]').innerText;
+         }
+       }
+     });
    }
 }
